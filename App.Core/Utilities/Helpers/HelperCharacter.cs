289f609177a14c05159644dd1ea8a1509aa24524
fatihtarakc <fatih_trkci96@hotmail.com>
2026-05-06 02:54:43 +0300

    generator method was writed.

--- App.Core/Utilities/Helpers/HelperCharacter.cs
+++ App.Core/Utilities/Helpers/HelperCharacter.cs
@@ -2,6 +2,8 @@
 {
     public static class HelperCharacter
     {
+        private static char[] upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        private static char[] lowerCharacters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
         private static char[] digits = "0123456789".ToCharArray();
         private static char[] turkishCharacters = { 'ç', 'Ç', 'ğ', 'Ğ', 'ı', 'İ', 'ö', 'Ö', 'ş', 'Ş', 'ü', 'Ü' };
         private static char[] symbols = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '\\', '|', ';', ':', '\'', '\"', ',', '.', '<', '>', '/', '?', '`', '~' };
@@ -13,5 +15,19 @@
 
         public static bool SymbolControl(string input) =>
             string.IsNullOrWhiteSpace(input) is true ? false : input.Any(character => symbols.Contains(character));
+
+        public static string Generator(int upperCharacter = 0, int lowerCharacter = 0, int turkishCharacter = 0, int digit = 0, int symbol = 0)
+        {
+            var random = new Random();
+            var code = new StringBuilder();
+
+            for (int i = 0; i < upperCharacter; i++) code.Append(upperCharacters[random.Next(upperCharacters.Length)]);
+            for (int i = 0; i < lowerCharacter; i++) code.Append(lowerCharacters[random.Next(lowerCharacters.Length)]);
+            for (int i = 0; i < turkishCharacter; i++) code.Append(turkishCharacters[random.Next(turkishCharacters.Length)]);
+            for (int i = 0; i < digit; i++) code.Append(digits[random.Next(digits.Length)]);
+            for (int i = 0; i < symbol; i++) code.Append(symbols[random.Next(symbols.Length)]);
+
+            return new string(code.ToString().OrderBy(orderBy => random.Next()).ToArray());
+        }
     }
 }
